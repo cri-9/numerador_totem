3cabbad95e4ab6491090e8ec632cc70b7f380155
César Eduardo López Fernández <cesar.lopez@mineduc.cl>
2025-03-19 10:18:17 -0300

    LDAP errores de navgadores

--- verificar_sesion.php
+++ verificar_sesion.php
@@ -1,12 +1,14 @@
 <?php
 session_start();
-header('Content-Type: application/json');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0"); //Evita que el navegador guarde en caché la página actual
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache"); //Evita que el naveador guarde la respuesta en Caché
+header('Content-Type: application/json'); //Refueza NO-CACHE para navegadores que no soportan Cache-Control
 
 if (isset($_SESSION['usuario'])) {
     echo json_encode([
         "sesion_activa" => true,
-        "usuario" => $_SESSION['usuario'],
-        "nombre_completo" => $_SESSION['nombre_completo'] ?? $_SESSION['usuario'] // Si no hay nombre, usa usuario
+        "nombre_completo" => $_SESSION['nombre_completo'] ?? $_SESSION['usuario']
     ]);
 } else {
     echo json_encode(["sesion_activa" => false]);
